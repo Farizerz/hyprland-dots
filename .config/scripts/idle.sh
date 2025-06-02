691d1cd3b0e6c8ae1d28143f2936a702cb61267c
Fariz R <fariz.rizki.ramadhan@gmail.com>
2025-06-02 14:37:57 +0700

    Update scripts & waybar configs

--- .config/scripts/idle.sh
+++ .config/scripts/idle.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
 if [ -f "/usr/bin/swayidle" ]; then
-  swayidle -w timeout 120 "hyprctl dispatch dpms off" resume "hyprctl dispatch dpms on"
+  swayidle -w \
+  timeout 120 "hyprctl dispatch dpms off" resume "hyprctl dispatch dpms on" \
+  timeout 600 "[ '$(cat /sys/class/power_supply/AC0/online) -eq 0 ] && systemctl suspend"
 else
-  echo "swayidle is not found"
+  echo "swayidle is not found";
 fi;
