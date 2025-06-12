e1711a71fe8746c65842ef630e6f4252bfb8de53
Fariz R <fariz.rizki.ramadhan@gmail.com>
2025-06-12 08:23:05 +0700

    Updated waybar & scripts, added forget wifi feature

--- .config/scripts/limit-tdp.sh
+++ .config/scripts/limit-tdp.sh
@@ -1,4 +1,4 @@
 while true; do
   sudo ryzenadj --stapm-limit=20000 --fast-limit=20000 --slow-limit=20000
-  sleep 10
+  sleep 15
 done
--- .config/scripts/toggle-monitor.sh
+++ .config/scripts/toggle-monitor.sh
@@ -2,7 +2,7 @@
 
 monitors=$(hyprctl monitors)
 
-if echo "$monitors" | grep -q "^Monitor eDP-1"; then
+if echo "$monitors" | grep -q "^Monitor eDP-1" && echo "$monitors" | grep -q "^Monitor HDMI-A-1"; then
   hyprctl keyword monitor "eDP-1,disable"
 else
   hyprctl keyword monitor "eDP-1,2880x1620@120,auto,1.5"
