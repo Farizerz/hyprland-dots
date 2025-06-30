af72fc3c405e1751ed84a84d9ed8ba386e45353a
Fariz R <farizerz@gmail.com>
2025-06-30 10:39:58 +0700

    Version 1.5.3

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 echo "Installing packages..."
 sleep 1
-bash ~/.config/scripts/install-packages.sh
+bash ~/.config/scripts/system-scripts/install-packages.sh
 sleep 1
 echo "Making scripts executable..."
 sleep 1
-bash ~/.config/scripts/execute-scripts.sh
+bash ~/.config/scripts/system-scripts/execute-scripts.sh
 sleep 1
 echo "Enabling services..."
 sleep 1
-sudo ~/.config/scripts/enable-services.sh
+sudo ~/.config/scripts/system-scripts/enable-services.sh
 sleep 1
 echo "Done."
