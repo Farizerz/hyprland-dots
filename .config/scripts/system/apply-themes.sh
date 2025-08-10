910e316496b53bf1d6308cf3f0037cbf3537c6ea
Fariz R <farizerz@gmail.com>
2025-08-10 14:55:09 +0700

    Version 1.6.2

---
+++ .config/scripts/system/apply-themes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+
+~/.config/scripts/theme/wallpaper.sh
