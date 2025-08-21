be223d0fc5983c25528c3e9046920c9620e76677
Fariz R <farizerz@gmail.com>
2025-08-22 01:56:09 +0700

    Version 1.8.1

--- .config/scripts/system/enable-services.sh
+++ .config/scripts/system/enable-services.sh
@@ -1,25 +1,30 @@
-sudo systemctl enable NetworkManager
+#!/bin/bash
+
+sudo systemctl enable --now NetworkManager
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now tlp
 sudo systemctl enable --now sddm
 
+USERNAME=$1
+
 ### For bypassing auth on battery preset services ###
-echo "$SUDO_USER ALL=(ALL) NOPASSWD: /home/$SUDO_USER/.config/scripts/rofi-battery.sh, /bin/cp, /usr/bin/tlp" | sudo tee /etc/sudoers.d/rofi-battery > /dev/null
+echo "$USERNAME ALL=(ALL) NOPASSWD: /home/$USERNAME/.config/scripts/rofi-battery.sh, /bin/cp, /usr/bin/tlp" | sudo tee /etc/sudoers.d/rofi-battery > /dev/null
 
 sudo chmod 0440 /etc/sudoers.d/rofi-battery
 
 ### For bypassing auth on custom boot & others ###
 sudo EDITOR="tee -a" visudo <<EOF
 
-$SUDO_USER ALL=(ALL) NOPASSWD: \\
-/home/$SUDO_USER/.config/scripts/system/custom-boot.sh, \\
-/usr/sbin/efibootmgr
+$USERNAME ALL=(ALL) NOPASSWD: \\
+/home/$USERNAME/.config/scripts/system/custom-boot.sh, \\
+/usr/sbin/efibootmgr, \\
+/usr/bin/ryzenadj
 EOF
 
 ### For virtual camera on OBS ###
 sudo modprobe v4l2loopback exclusive_caps=1 card_label="OBS Virtual Camera"
 
 ### Change hyprland boot service using custom script ###
-sudo ~/.config/scripts/system/custom-boot.sh 
+sudo ~/.config/scripts/system/custom-boot.sh
 
 echo "✅ Services enabled and started."
--- .config/scripts/system/install-packages.sh
+++ .config/scripts/system/install-packages.sh
@@ -1,24 +1,67 @@
 #!/bin/bash
 
-USERNAME=$(whoami)
+USERNAME=$1
 
 # Packages to install
 pacman=(
+  # Core System / Utilities
+  pacman-contrib
+  parallel
+  jq
+  bc
+  exa
+  nano
+  nvim
+  smartmontools
+  openssh
+  ryzenadj
+  # Display Server / Wayland / Hyprland Core
+  xorg-server
+  xorg-xinit
   hyprland
+  hyprpaper
+  hypridle
   sddm
   uwsm
   waybar
-  hyprpaper
   swww
   hyprlock
-  alacritty
-  ttf-jetbrains-mono-nerd
+  rofi-wayland
+  rofi-emoji
+  slurp
+  grim
+  swappy
+  wl-clipboard
+  qt5-wayland
+  qt6-wayland
+  qt6-svg
+  qt6-virtualkeyboard
+  qt6-multimedia-ffmpeg
+  xdg-desktop-portal-hyprland
+  xdg-desktop-portal-gtk
+  xdg-user-dirs
+  xdg-utils
+  # Networking / Connectivity
   networkmanager
-  network-manager-applet
   bluez
   bluez-utils
-  bc
+  wpa_supplicant
+  # Audio / Multimedia
+  pipewire
+  pipewire-alsa
+  pipewire-pulse
+  wireplumber
+  pavucontrol
+  easyeffects
   mpv
+  ffmpeg
+  ffmpegthumbnailer
+  cava
+  playerctl
+  obs-studio
+  yt-dlp
+  chafa
+  # File Management / System Tools
   thunar
   thunar-volman
   gvfs
@@ -28,87 +71,85 @@ pacman=(
   polkit
   polkit-kde-agent
   polkit-gnome
-  qt5-wayland
-  qt6-wayland
-  qt6-svg 
-  qt6-virtualkeyboard 
-  qt6-multimedia-ffmpeg
-  xdg-desktop-portal-hyprland
-  xdg-desktop-portal-gtk
-  xdg-user-dirs
-  xdg-utils
-  pacman-contrib
-  parallel
-  jq
   ntfs-3g
   tumbler
-  ffmpegthumbnailer
-  rofi-wayland
-  rofi-emoji
-  fastfetch
-  dunst
-  btop
-  pipewire
-  pipewire-alsa
-  pipewire-pulse
-  wireplumber
-  pavucontrol
-  easyeffects
-  tlp
-  slurp
-  grim
-  swappy
-  wl-clipboard
-  gwenview
-  nwg-look
-  nwg-displays
   file-roller
-  mousepad
+  # Fonts / Themes / Appearance
+  ttf-jetbrains-mono-nerd
   papirus-icon-theme
   noto-fonts
   noto-fonts-cjk
   noto-fonts-emoji
-  swayidle
+  nwg-look
+  nwg-displays
   brightnessctl
-  playerctl
-  imagemagick
+  fastfetch
+  dunst
+  # Applications / Productivity
+  alacritty
+  eog
+  mousepad
   nvim
-  nano
   chromium
-  cava
-  obs-studio
   libreoffice-still
-  yt-dlp
-  chafa
-  openssh
-  smartmontools
-  wget
-  network-manager-applet
-  wpa_supplicant
-  exa
   android-tools
   scrcpy
-  xorg-server
-  xorg-xinit
-  # intel graphics
-  intel-media-driver
-  libva-intel-driver
-  vulkan-intel
-  # AMD graphics
-  libva-mesa-driver
-  mesa
-  vulkan-radeon
-  xf86-video-amdgpu
-  xf86-video-ati
-  # Nvidia graphics
+  imagemagick
+  btop
+)
+
+pacman_nvidia=(
   libva-nvidia-driver
   dkms
   nvidia-dkms
   nvidia-utils
+  nvidia-settings
+  egl-wayland
+  opencl-nvidia
 )
 
+pacman_amd=(
+  libva-mesa-driver
+  mesa
+  mesa-utils
+  vulkan-radeon
+  xf86-video-amdgpu
+  xf86-video-ati
+)
+
+pacman_intel=(
+  intel-media-driver
+  libva-intel-driver
+  vulkan-intel
+)
+
+pacman_vm=(
+  mesa
+)
+
+while true; do
+  echo "Select your GPU driver:"
+  echo "1) Nvidia"
+  echo "2) AMD"
+  echo "3) Intel"
+  echo "4) Virtual Machine"
+  echo "5) Other (install all drivers)"
+  read -rp "Enter choice [1-4]: " choice < /dev/tty
+
+  case "$choice" in
+    1) gpu_packages=("${pacman_nvidia[@]}"); break ;;
+    2) gpu_packages=("${pacman_amd[@]}"); break ;;
+    3) gpu_packages=("${pacman_intel[@]}"); break ;;
+    4) gpu_packages=("${pacman_vm[@]}"); break ;;
+    5) gpu_packages=("${pacman_intel[@]}" "${pacman_amd[@]}" "${pacman_nvidia[@]}"); break ;;
+    *) echo "Invalid choice. Please enter 1, 2, 3, 4, or 5." ;;
+  esac
+done
+
+all_pacman=("${pacman[@]}" "${gpu_packages[@]}")
+
 # Install packages using pacman
-sudo pacman -S --noconfirm --needed "${pacman[@]}"
+sudo pacman -S --noconfirm --needed "${all_pacman[@]}"
 
 yay=(
   pinta
@@ -151,7 +192,7 @@ sudo mv /usr/share/icons/Papirus /usr/share/icons/Papirus-Dark /usr/share/icons/
 
 sudo chown -R "$USERNAME":"$USERNAME" $HOME/.local
 
-# # Post-installation steps - Applying sddm theme
+# Post-installation steps - Applying sddm theme
 sudo cp -r /home/$USERNAME/.local/share/sddm/themes /usr/share/sddm
 
 sudo cp -r /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
@@ -164,6 +205,22 @@ sudo tee /etc/sddm.conf.d/virtualkbd.conf > /dev/null <<EOF
 [General]
 InputMethod=qtvirtualkeyboard
 EOF
+
+# Post-installation steps - Detecting monitor sources
+CONF="$HOME/.config/hypr/hyprconfig/monitor-sources.conf"
+
+monitors=($(xrandr --query | grep " connected" | cut -d' ' -f1))
+
+cat > "$CONF" <<'EOF'
+# This is the source of anything related to laptop monitor scripts, you can (and should) change this according to your setup.
+# The laptop monitor in monitors.conf will be replaced by these values everytime the pc boots, changing themes,
+# or closing / opening laptop lid.
+
+EOF
+
+for mon in "${monitors[@]}"; do
+    echo "monitor=$mon,preferred,auto,auto" >> "$CONF"
+done
 
 # Finished
 
