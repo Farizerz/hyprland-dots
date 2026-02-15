0e5036da20f3d3ffbd0bb5be99641a93065ecd7b
Fariz R <farizerz@gmail.com>
2026-02-15 15:55:21 +0700

    Version 1.9.3

--- .config/scripts/system/custom-boot.sh
+++ .config/scripts/system/custom-boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Change from regular boot (Exec=Hyprland) into custom boot using boot.sh
 
-if grep -q "^Exec=Hyprland$" /usr/share/wayland-sessions/hyprland.desktop; then
-    sudo sed -i "s|^Exec=Hyprland$|Exec=/home/$SUDO_USER/.config/scripts/system/boot.sh|" /usr/share/wayland-sessions/hyprland.desktop
+if grep -q "^Exec=start-hyprland$" /usr/share/wayland-sessions/hyprland.desktop; then
+  sudo sed -i "s|^Exec=start-hyprland$|Exec=/home/$SUDO_USER/.config/scripts/system/boot.sh|" /usr/share/wayland-sessions/hyprland.desktop
 fi
